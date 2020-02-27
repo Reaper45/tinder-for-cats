ef760f6a4670c0a8dedf32bed2aea0c5ab33614b
Joram <jomwashighadi@gmail.com>
2020-02-27 17:46:31 +0300

    Cat UI

--- src/components/Cat.tsx
+++ src/components/Cat.tsx
@@ -1,11 +1,78 @@
 import * as React from "react";
+import styled from "styled-components";
+
+import likeIcon from "static/like.png";
+import disLikeIcon from "static/dislike.png";
 
 interface ICatProps {
-  cat: CatType
+  cat?: CatType
 }
 
+const CatWrapper = styled("div")`
+  box-shadow: 0 3px 14px ${props => props.theme.colors.light};
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column
+`;
+
+const Image = styled("div")<{ img?: string }>`
+  border-radius: 8px;
+  background: ${props => `url(${props.img}) ${props.theme.colors.dark}`};
+  height: 100%;
+  width: 100%;
+  display: block;
+  flex-grow: 1;
+`;
+
+const Actions = styled("div")`
+  background: transparent;
+  padding: 1em 0;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+`;
+
+const Button = styled("button")`
+  background: #fff;
+  justify-content: space-around;
+  border: none;
+  box-shadow: 3px 3px 14px ${props => props.theme.colors.light};
+  outline: none;
+  border-radius: 50%;
+  padding: 1.2rem;
+  text-align: center;
+  transition: all 200ms ease-in-out;
+  cursor: pointer;
+  margin: 0 1em;
+  :hover {
+    transform: scale(1.1);
+    box-shadow: 0px 0px 14px ${props => props.theme.colors.light};
+  }
+  > img {
+    width: 20px;
+    vertical-align: middle;
+    line-height: 1;
+  }
+`;
+
 const Cat: React.FC<ICatProps> = ({ cat }) => {
-  return <div></div>;
+  return (
+    <CatWrapper>
+      <Image />
+      <Actions>
+        <Button title="Nope">
+          <img src={disLikeIcon} alt="Nope" />
+        </Button>
+        <Button title="Like">
+          <img src={likeIcon} alt="Like" />
+        </Button>
+      </Actions>
+    </CatWrapper>
+  );
 };
 
 export default Cat;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,46 @@
 import * as React from "react";
+import styled from "styled-components";
+import { useState, useEffect } from "react";
 
-// import { ICat } from "components/Cat";
 import { makeGetRequest } from "lib/utils";
+import Cat from "components/Cat";
+
+const HomeWrapper = styled("div")`
+  height: 100%;
+  width: 100%;
+`;
+
+const CatsContainer = styled("div")`
+  width: 40%;
+  height: 100%;
+  margin: 0 auto;
+  padding: 3em 0;
+  box-sizing: border-box;
+  display: block;
+`;
 
 interface IHomeState {
   cats: CatType[] | any[];
 }
 
-class Home extends React.Component<{}, IHomeState> {
-  constructor(props: any) {
-    super(props)
-    this.state = { cats: [] }
-  }
-  public componentDidMount() {
-    this.getCats();
-  }
-  public getCats = async () => {
-    const response = await makeGetRequest();
-    const cats = await response.json();
-    console.log({ cats });
-  }
-  public render() {
-    return <div>Home</div>;
-  }
+const Home: React.FC = () => {
+  const [cats, setCats] = useState<CatType[]>([]);
+  useEffect(() => {
+    const getCats = async () => {
+      const response = await makeGetRequest();
+      const cats = await response.json();
+      setCats(cats);
+    }
+    getCats();
+  });
+  console.log(cats)
+  return (
+    <HomeWrapper>
+      <CatsContainer>
+        <Cat />
+      </CatsContainer>
+    </HomeWrapper>
+  );
 };
 
 export default Home;
