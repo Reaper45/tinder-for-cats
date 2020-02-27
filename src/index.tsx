d237153ec13d188e397180daefa22234c08d807a
Joram <jomwashighadi@gmail.com>
2020-02-27 16:06:21 +0300

    complete setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import { ThemeProvider } from "styled-components";
 
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { theme } from "./theme";
+
+import "./index.css";
 
 const history = createBrowserHistory();
 
 ReactDOM.render(
   <Router history={history}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Router>,
   document.getElementById("root")
 );
---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+
+interface ITheme {
+  primary: string; 
+  dark: string; 
+  light: string; 
+  secondary: string; 
+}
+
+export const theme: ITheme = {
+  primary: "#805AD5",
+  dark: "#44337A",
+  light: "#FAF5FF",
+  secondary: "#222222"
+};
